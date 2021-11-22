d8daa43fc0a0a0e73282cf269ada4a19b9298bc9
Wolfram Diestel <wolfram.diestel@t-systems.com>
2021-11-22 20:52:54 +0100

    +test.sh

---
+++ tst/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -L "http://localhost:8000/cikado/cikado?sercho=hundo&kie=klasikaj"
+
+curl -L "http://localhost:8000/cikado/cikado?sercho=+hundo&kie=klasikaj"
+
+curl -L "http://localhost:8000/cikado/cikado?sercho=\bhun[dt]o&kie=klasikaj"
