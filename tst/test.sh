7d55c5057b2433669ca06fb54704061bb6b29442
Wolfram Diestel <wolfram.diestel@t-systems.com>
2022-01-11 17:05:44 +0100

    +test verkaro

--- tst/test.sh
+++ tst/test.sh
@@ -5,3 +5,5 @@ curl -L "http://localhost:8000/cikado/cikado?sercho=hundo&kie=klasikaj"
 curl -L "http://localhost:8000/cikado/cikado?sercho=+hundo&kie=klasikaj"
 
 curl -L "http://localhost:8000/cikado/cikado?sercho=\bhun[dt]o&kie=klasikaj"
+
+curl -L "http://localhost:8000/cikado/verkaro?kiu=klasikaj"
